b82da89a2ec78185f0964c914f875c5e5e2359d2
Gamze Dede Pala <gamze.dedepala@gmail.com>
2022-12-21 00:52:13 +0100

    Added secrets to be fetched from cicd

--- src/test/java/helpers/authentication .js
+++ src/test/java/helpers/authentication .js
@@ -4,19 +4,3 @@ function fn(creds) {
     var encoded = Base64.getEncoder().encodeToString(temp.toString().getBytes());
     return 'Basic ' + encoded;
   }
-
-  public static String getAccessToken() {
-    Response response =
-            SerenityRest.given().auth().preemptive().basic(clientId, clientSecret)
-                    .formParam("grant_type", "password")
-                    .formParam("username", username)
-                    .formParam("password", password)
-                    .formParam("scope", scope)
-                    .when()
-                    .post(tokenUri);
-
-    JSONObject jsonObject = new JSONObject(response.getBody().asString());
-    String accessToken = jsonObject.get("access_token").toString();
-
-    return accessToken;
-}
